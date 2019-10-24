6de9acba8262728ee71ef3a28965a6eaa9eb382d
MariaTkachenkoV <parturnaks23@gmail.com>
2019-10-24 09:04:06 +0300

    commit

--- Ind.LB1.M/Program.cs
+++ Ind.LB1.M/Program.cs
@@ -24,7 +24,7 @@ namespace Ind.LB1.M
                        Film3 = Console.ReadLine();
                 Console.Clear();
                 Console.WriteLine(" __________________________________________________________________________________ ");
-                Console.WriteLine("|{0,-20}", "Кинопродукция                                                                     |");
+                Console.WriteLine("|{0,0}", "Кинопродукция                                                                     |");
                 Console.WriteLine("|__________________________________________________________________________________|");
                 Console.WriteLine("|{0,-30}|{1,-30}|{2,-15}|{3,-4}|", "Фильм", "Режиссер", "Год выпуска", "Тип");
                 Console.WriteLine("|______________________________|______________________________|_______________|____|");
@@ -56,14 +56,14 @@ namespace Ind.LB1.M
                 Console.WriteLine("Введите перемнную b");
                 double b = double.Parse(Console.ReadLine());
                 double z = (Math.Sin(x) / (Math.Sqrt(Math.Pow(m, 2) + Math.Pow(Math.Sin(x), 2)))) - (с * m *( Math.Log(m*x, Math.E)));
-                if((Math.Sqrt(Math.Pow(m, 2) + Math.Pow(Math.Sin(x), 2)))<0)
+                if((Math.Pow(m, 2) + Math.Pow(Math.Sin(x), 2))<0)
                 {
                     Console.WriteLine("Одно из значений не входит в ОДЗ");
                     Environment.Exit(0);
                 }
                 double s = (Math.Pow(Math.E, (-a * x)) * (Math.Sqrt(x + 1)) + (Math.Pow(Math.E, (-b * x))) * (Math.Sqrt(x + 1.5)));
-                if (Math.Sqrt(x + 1) < 0 &&
-                    Math.Sqrt(x + 1.5) < 0)
+                if (x < -1 &&
+                    x < -1.5)
                 {
                     Console.WriteLine("Одно значений не входит в ОДЗ");
                     Environment.Exit(0);
